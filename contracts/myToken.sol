197a1d633c05de512b8a285ec8896e4a87230aa1
Vimlesh  Kumar Pal <111525169+vb756652@users.noreply.github.com>
2023-08-17 12:43:57 +0530

    Update myToken.sol

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -1,34 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
-
 contract Token {
-
     // public variables here
-    string public tokenName = "RACING";
-    string public abbrv = "RCR";
+    string public tokenName = "RAMANUJA";
+    string public abbrv = "LKP";
     uint public totalSupply = 0;
-    
+    address public owner;
 
     // mapping variable here
-    mapping (address=> uint) public balances;
+    mapping(address => uint) public balances;
 
-    // mint function
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;  
-
+    // Modifier to check if the caller is the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
     }
 
+    // Constructor to set the owner
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // mint function
+    function mint(address _address, uint _value) public onlyOwner {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
 
     // burn function
-    function burn( address _address,uint _value) public {
-        if( balances[_address] >= _value){
+    function burn(address _address, uint _value) public onlyOwner {
+        if (balances[_address] >= _value) {
             totalSupply -= _value;
             balances[_address] -= _value;
         }
-        
     }
 
+    // transfer function
+    function transfer(address _to, uint _value) public {
+        require(_to != address(0), "Invalid address");
+        require(_value <= balances[msg.sender], "Insufficient balance");
+        
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+    }
 }
